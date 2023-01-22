bc8ee2351adeaf13848378fffc18bf9655a839e5
alexandermarin <alexander.marin@student.griffith.ie>
2023-01-22 15:28:53 +0000

    init - Created .NET Core 6 MVC Project

---
+++ RestaurantDecider/RestaurantDecider/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantDecider.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
