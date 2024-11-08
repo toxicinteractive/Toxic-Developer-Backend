34b45edd72d0e2b386781baeccabdf2274db7e36
Hanna Ireflo <floengard@hotmail.com>
2024-11-08 13:27:19 +0100

    The commit include a new .NET Core Web API project. The API include two HTTP get methods and two HTTP Post Methods, GetRestaurants, GetRestaurantOfTheDay, AddRestaurant, AddOpenHours. The data is structured with Entity Framework and UseInMemoryDatabase() is used to handle the data without connection to a database. Some Mocked data is added so it will be easy to test the API. In Program.cs is swagger UI declared to be used as for testing.

---
+++ RestaurantAPI/RestaurantAPI/Models/RestaurantContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Models;
+
+public class RestaurantContext : DbContext
+{
+    public RestaurantContext(DbContextOptions<RestaurantContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<RestaurantItem> RestaurantItems { get; set; } = null!;
+    public DbSet<OpenHoursItem> OpenHoursItems { get; set; } = null!;
+    public DbSet<UsedRandomRestaurantItem> UsedRandomRestaurantItems { get; set; } = null!;
+}
---
+++ RestaurantAPI/RestaurantAPI/Models/RestaurantItem.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
+using static System.Net.WebRequestMethods;
+
+namespace RestaurantAPI.Models
+{
+    public class RestaurantItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ForeignKey("OpenHoursItem")]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Food { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string GoogleMapsUrl
+        {
+            get
+            {
+                return "https://www.google.com/maps/dir/?api=1&destination=+" + Address + "+" + City;
+            }
+        }
+        public List<OpenHoursItem> OpenHours { get; set; } = new();
+    }
+
+    [PrimaryKey(nameof(RestaurantId), nameof(Day))]
+    public class OpenHoursItem
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int RestaurantId { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public DayOfWeek Day { get; set; }
+        public string Open { get; set; }
+        public string Close { get; set; }
+    }
+
+
+    public class UsedRandomRestaurantItem
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
